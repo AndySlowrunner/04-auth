c714cf57e73d43559ae6a69e3f88c2b0469a670e
Andy <andyfartushniy@gmail.com>
2023-12-27 20:10:10 +0200

    Updated

---
+++ models/User.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+
+const userSchema = new Schema({
+    password: {
+        type: String,
+        required: [true, 'Set password for user'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    subscription: {
+        type: String,
+        enum: ["starter", "pro", "business"],
+        default: "starter"
+    },
+    token: String
+});
+
+const User = model("user", userSchema);
+
+export default User;
